fcfb45f2fbdb22f3520e6551042f89e3b9390118
dgaujero <deion@aujero.net>
2020-02-22 11:02:21 -0500

    node server runs react now

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port: http://localhost:${port}`));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
