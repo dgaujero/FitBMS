66540abd7467ff316f69e743595cbabda530ed70
dgaujero <deion@aujero.net>
2020-02-23 16:29:33 -0500

    deleted routes cuz we're using controller

--- models/post.js
+++ models/post.js
@@ -1,10 +1,5 @@
 var orm = require("../config/orm");
 var admin = {
-//     create: function(cols, vals, cb) {
-//         orm.create("users", cols, vals, function(res) {
-//             cb(res);
-//     });
-// },
     all: function(cb) {
         orm.all("checkInTable", function(res) {
             cb(res);
