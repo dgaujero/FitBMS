040ed10fb711b21fce9967e0cdae8afb3f75c485
Claudia Gutarra <claudiaagutarra@gmail.com>
2020-02-29 20:00:13 -0500

    added navbar and routes

--- client/src/components/Manage/trainers.js
+++ client/src/components/Manage/trainers.js
@@ -1,42 +1,43 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import './manage.css';
 
 class Trainers extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            trainers: [],
-            // id: "",
-            firstName: "",
-            lastName: ""
-        }
-    };
-    componentDidMount() {
-        this.loadTrainers();
+  constructor(props) {
+    super(props);
+    this.state = {
+      trainers: [],
+      // id: "",
+      firstName: "",
+      lastName: ""
     }
-    loadTrainers = () => {
-        axios.get('/trainers')
-          .then((response) => {
-            console.log(response)
-            this.setState({trainers: response.data.trainers})
-          })
-      };
-    renderTrainer = () => {
-        return this.state.trainers.map(trainer => (
-          <div key={trainer.id}>
-            {trainer.firstName} {trainer.lastName}
-          </div>
-        ))
-      };
-   
-    render() {
-        return (
-            <div>
-                 <h1>Trainers</h1>
+  };
+  componentDidMount() {
+    this.loadTrainers();
+  }
+  loadTrainers = () => {
+    axios.get('/trainers')
+      .then((response) => {
+        console.log(response)
+        this.setState({ trainers: response.data.trainers })
+      })
+  };
+  renderTrainer = () => {
+    return this.state.trainers.map(trainer => (
+      <div key={trainer.id}>
+        {trainer.firstName} {trainer.lastName}
+      </div>
+    ))
+  };
+
+  render() {
+    return (
+      <div className="trainers-container">
+        <h1>Trainers</h1>
         {this.renderTrainer()}
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 export default Trainers;
 
