5e6fe377a7cd6320f031fb7cc9f24b8ff08aeb4c
Christian <chrell827@gmail.com>
2020-03-03 00:33:44 -0500

    added profile pic functionality, added change email/password through firebase, added alerts

--- database/schema.sql
+++ database/schema.sql
@@ -1,38 +1,48 @@
 DROP DATABASE IF EXISTS bmsDB;
 CREATE DATABASE bmsDB;
-
 USE bmsDB;
-
 CREATE TABLE checkInTable
 (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
     purpose varchar(255) NOT NULL,
     seshTime timestamp default CURRENT_TIMESTAMP,
     uId varchar(100) NOT NULL,
-	PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
-
 CREATE TABLE membersTable
 (
-	id int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     firstName varchar(255) NOT NULL,
     lastName varchar(255) NOT NULL,
+    profilePic varchar(400) NULL,
     phoneNum BIGINT NOT NULL,
     bday date NOT NULL,
     address varchar(255) NOT NULL,
     emergName varchar(255),
-    ePhoneNum BIGINT,
+    ePhoneNum BIGINT NULL,
     uId varchar(100) NOT NULL,
     isTrainer boolean default '0',
-	PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 CREATE TABLE classesTable
 (
-	id int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-	className varchar(255) NOT NULL,
+    className varchar(255) NOT NULL,
     classType varchar(255) NOT NULL,
-	assignedTrainer varchar(255) NOT NULL,
+    assignedTrainer varchar(255) NOT NULL,
     classSize int NOT NULL
 );
+INSERT INTO membersTable (firstName, lastName, profilePic, phoneNum, bday, address, emergName, uId) 
+VALUES (
+    'Aldo',
+    'Huayhua',
+    'https://i.ibb.co/4mLTzv0/aldo-huayhua.png',
+    '1111111111',
+    '1997-11-02',
+    '161 Newkirk Ave',
+    'mom',
+    'bRYIdls4DndXxQABPAf9SueTMNh1'
+    );
+
