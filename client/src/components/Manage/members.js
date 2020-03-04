11566b3104370dd9ca193a475a04406caa7ce2aa
dgaujero <deion@aujero.net>
2020-03-03 19:56:11 -0500

    search bar for members in manage works

--- client/src/components/Manage/members.js
+++ client/src/components/Manage/members.js
@@ -15,7 +15,8 @@ class Members extends Component {
     super(props);
     this.state = {
       members: [],
-      id: ""
+      id: "",
+      search: ""
     }
 
   };
@@ -38,15 +39,23 @@ class Members extends Component {
       .catch(err => console.log(err));
   };
 
+  updateSearch(event) {
+    this.setState({search: event.target.value.substr(0, 5)})
+  }
+
   render() {
+    let filteredList = this.state.members.filter(
+      (member) => { 
+          return member.firstName.toLowerCase().indexOf(this.state.search) === 0;
+      }
+    );
     return (
       <div className="members-container">
         <h1>Members</h1>
-
-
+        <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)} placeholder="Search..."></input>
         {this.state.members.length ? (
           <Row>
-            {this.state.members.map(member => {
+            {filteredList.map(member => {
               return (
 
                 <Card key={member.id} className="card-container">
